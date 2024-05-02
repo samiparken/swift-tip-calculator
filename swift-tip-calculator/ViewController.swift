9d11935aa39a4de0069ee786218b76ddfed80734
samiparken <samiparken@gmail.com>
2024-05-02 10:28:07 +0200

    replace project with StoryBoard

---
+++ swift-tip-calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-tip-calculator
+//
+//  Created by Han-Saem Park on 2024-05-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
