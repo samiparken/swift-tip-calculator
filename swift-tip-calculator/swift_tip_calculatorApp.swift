68d925538f0b1342f11590f127fec5f124165c00
samiparken <samiparken@gmail.com>
2024-05-02 09:27:52 +0200

    Initial Commit

---
+++ swift-tip-calculator/swift_tip_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_tip_calculatorApp.swift
+//  swift-tip-calculator
+//
+//  Created by Han-Saem Park on 2024-05-02.
+//
+
+import SwiftUI
+
+@main
+struct swift_tip_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
