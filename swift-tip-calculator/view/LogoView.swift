b8b1b232549dcf3e2baf554ec02d31b7fc88efa0
samiparken <samiparken@gmail.com>
2024-05-02 15:51:03 +0200

    Add LabelFactory

--- swift-tip-calculator/view/LogoView.swift
+++ swift-tip-calculator/view/LogoView.swift
@@ -9,6 +9,39 @@ import UIKit
 
 class LogoView: UIView {
 
+    private let imageView: UIImageView = {
+        let view = UIImageView(image: .init(named: "icCalculatorBW"))
+        view.contentMode = .scaleAspectFit
+        return view
+    }()
+    
+    private let topLabel: UILabel = {
+        let label = UILabel()
+        let text = NSMutableAttributedString(
+            string: "Mr TIP",
+            attributes: [.font: ThemeFont.demibold(ofSize: 16)])
+        text.addAttributes([.font: ThemeFont.bold(ofSize: 24)], range: NSMakeRange(3, 3)) //applied different text size
+        label.attributedText = text
+        return label
+    }()
+    
+    private let bottomLabel: UILabel = {
+       
+        
+    }()
+    
+    private lazy var hStackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [
+        
+        ])
+        view.axis = .horizontal
+        view.spacing = 8
+        view.alignment = .center
+        return view
+    }()
+    
+    
+    
     init() {
         super.init(frame: .zero)
         layout()
@@ -18,6 +51,8 @@ class LogoView: UIView {
     }
     
     private func layout() {
-        backgroundColor = .red
+
+
+
     }
 }
